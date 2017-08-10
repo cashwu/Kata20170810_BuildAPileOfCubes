fb3d18578dbfad4210b460931dcf784f67c81add
cash <cash@cashwu.com>
2017-08-10 19:26:40 +0800

    init

--- Kata20170810_BuildAPileOfCubes/BuildAPileOfCubesTests.cs
+++ Kata20170810_BuildAPileOfCubes/BuildAPileOfCubesTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170810_BuildAPileOfCubes
 {
     [TestClass]
-    public class UnitTest1
+    public class BuildAPileOfCubesTests
     {
         [TestMethod]
         public void TestMethod1()
